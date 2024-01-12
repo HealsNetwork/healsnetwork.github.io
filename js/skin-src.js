d1c1abe7158f491ca5476a43aaa04298bc84e418
Filippo Marcon <f.marcon@donboscosandona.it>
2024-01-12 18:46:12 +0000

    Jan 12, 2024, 7:46 PM

--- js/skin-src.js
+++ js/skin-src.js
@@ -1,8 +1,6 @@
-const images = document.querySelectorAll(".col-lg-6 img");
+const cols = document.querySelectorAll(".col");
 
-for (const image of images) {
-  const h4 = image.parentNode.querySelector("h4");
-  const text = h4.textContent;
-
-  image.src = `https://mc-heads.net/body/${text}`;
+for (const col of cols) {
+  const imageUrl = `https://mc-heads.net/body/${col.textContent}`;
+  col.querySelector("img").src = imageUrl;
 }
