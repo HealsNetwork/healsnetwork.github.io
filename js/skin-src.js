eee7744c42ef588cbaa6bfe41d5b9cc047ce26e5
Filippo Marcon <f.marcon@donboscosandona.it>
2024-01-12 18:54:33 +0000

    Jan 12, 2024, 7:54 PM

--- js/skin-src.js
+++ js/skin-src.js
@@ -1,6 +1,16 @@
-const cols = document.querySelectorAll(".col");
+function changeImageLinkText() {
+  // Ottieni tutti gli elementi col
+  const cols = document.querySelectorAll(".col");
 
-for (const col of cols) {
-  const imageUrl = `https://mc-heads.net/body/${col.textContent}`;
-  col.querySelector("img").src = imageUrl;
+  // Per ogni elemento col
+  for (const col of cols) {
+    // Ottieni il testo dall'elemento h4
+    const text = col.querySelector("h4").textContent;
+
+    // Cambia il testo del link dell'immagine
+    col.querySelector("img").src = `https://mc-heads.net/body/${text}`;
+  }
 }
+
+// Avvia la funzione al caricamento della pagina
+document.addEventListener("DOMContentLoaded", changeImageLinkText);
